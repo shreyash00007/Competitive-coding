fe639d2ec57d703d525027b1ccf0abe742e1df63
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-19 20:31:55 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream f("data2.in");
+ofstream g("data.output");
+
+int main()
+{
+    int a, b, sum;
+     f>>a>>b;
+     sum = a + b;
+     g<<sum;
+    return 0;
+}
