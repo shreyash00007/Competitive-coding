dfe79805f9ab32b1677776e2b0e10f1f4e7da292
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-23 09:02:25 +0530

    Update Global_&_Local.cpp

--- Global_&_Local.cpp
+++ Global_&_Local.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-int Aglobal[3];
+int Aglobal[3]; // Global variable
 
 int main()
 {
-    int Alocal[3];
+    int Alocal[3]; // Local variable
     cout<<"Aglobal is:"<<Aglobal[0]<<" "<<Aglobal[1]<<" "<<Aglobal[2]<<"\n";
     cout<<"Alocal is:"<<Alocal[0]<<" "<<Alocal[1]<<" "<<Alocal[2]<<"\n";
     return 0;
