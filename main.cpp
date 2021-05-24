37aede57abed8a80005382cc9645662c34ce7f5c
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-24 16:13:15 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int number = 3;
+    double cubicRoot;
+
+    cubicRoot = pow((double)(number), 1.0/3);
+
+    cout<<fixed<<setprecision(10)<<cubicRoot<<"\n";
+    return 0;
+}
