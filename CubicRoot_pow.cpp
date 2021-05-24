1c563dd04fb085c68a709e221397b35027e76c24
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-24 16:14:40 +0530

    Update and rename main.cpp to CubicRoot_pow.cpp

--- CubicRoot_pow.cpp
+++ CubicRoot_pow.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    int number = 3;
-    double cubicRoot;
+    int number = 3; //variable
+    double cubicRoot; // declared as double 0.00
 
-    cubicRoot = pow((double)(number), 1.0/3);
+    cubicRoot = pow((double)(number), 1.0/3);     // used Pow 
 
     cout<<fixed<<setprecision(10)<<cubicRoot<<"\n";
     return 0;
