250ed82dc4fd53475fa20f5d59da2573e10bd267
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-25 08:55:15 +0530

    Update STL_for_Sort.cpp

--- STL_for_Sort.cpp
+++ STL_for_Sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int A[]={1, 4, 2, 0, 5, 10, 9};
-    sort(A+0, A+7);
+    sort(A+0, A+7); // Using the sort feature fro standard libary template
     for (int i=0;i<=6; ++i)
         cout<<A[i]<<" ";
     return 0;
@@ -18,7 +18,7 @@ bool cmp (int X, int Y)
 int main()
 {
     int A[]={1, 4, 2, 0, 5, 10, 9};
-    sort(A+0, A+7,cmp);
+    sort(A+0, A+7,cmp);  // Use of sort 
     for (int i=0;i<=6; ++i)
         cout<<A[i]<<" ";
     return 0;
