7f94ce36f6bdf90bf69e207da02bb55656839333
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2021-05-25 08:53:47 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A[]={1, 4, 2, 0, 5, 10, 9};
+    sort(A+0, A+7);
+    for (int i=0;i<=6; ++i)
+        cout<<A[i]<<" ";
+    return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+bool cmp (int X, int Y)
+{
+    return X > Y;
+}
+int main()
+{
+    int A[]={1, 4, 2, 0, 5, 10, 9};
+    sort(A+0, A+7,cmp);
+    for (int i=0;i<=6; ++i)
+        cout<<A[i]<<" ";
+    return 0;
+}
